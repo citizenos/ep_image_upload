d9ad23e46fe500db7f47f1656a9ca48bae28d0a5
John McLear <john@mclear.co.uk>
2021-02-09 20:45:51 +0000

    introduce additional content collection and some placeholder values

--- static/js/contentCollection.js
+++ static/js/contentCollection.js
@@ -1,12 +1,33 @@
 'use strict';
 
+// When an image is detected give it a lineAttribute
+// of Image with the URL to the iamge
 exports.collectContentImage = (name, context) => {
   const tname = context.tname;
-  const lineAttributes = context.state.lineAttributes;
+  const state = context.state;
+  const lineAttributes = state.lineAttributes;
   if (tname === 'div' || tname === 'p') {
     delete lineAttributes.img;
   }
   if (tname === 'img') {
-    lineAttributes.img = context.node.outerHTML;
+    if (context.node.currentSrc) {
+      // client
+      lineAttributes.img = context.node.currentSrc;
+    } else {
+      // server
+      lineAttributes.img = context.node.outerHTML;
+    }
+  }
+};
+
+exports.collectContentPre = (name, context) => {
+};
+
+exports.collectContentPost = (name, context) => {
+  const tname = context.tname;
+  const state = context.state;
+  const lineAttributes = state.lineAttributes;
+  if (tname === 'img') {
+    delete lineAttributes.img;
   }
 };
